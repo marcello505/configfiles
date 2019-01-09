4767acef7339f3c89ffbdcf0aed68c8f5b0cfbdf
marcello505 <32366042+marcello505@users.noreply.github.com>
2019-01-09 23:41:27 +0100

    Delete randombackground.sh

--- .scripts/randombackground.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Small script that changes the background every hour using pywall
-while [ true ]
-do
-#!/bin/bash
-DIR="/home/$USER/Pictures/Backgrounds"
-wal -g -i "$DIR"
-sleep 3600
-done
