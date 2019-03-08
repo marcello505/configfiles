39aa354127179560ca8413c6e6e8e2c2e4b26599
Marcello505 <marcellohaddeman@gmail.com>
2019-03-09 00:35:53 +0100

    Moved over to Arch Linux
    I finally moved over from Antergos to Arch Linux. Most stuff doesn't change except new console font, some Antergos specific options and using emoji's instead of unicode symbols. I might change this back to unicode symbols.

--- .scripts/rsync.sh
+++ .scripts/rsync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Shell script to run all my rsync commands.
 HOME="/home/$USER"
-GITDIR="/home/$USER/Documents/GitHub/configfiles"
+GITDIR="/home/$USER/GitHub/configfiles"
 
 # Github repo files
 rsync -rtuv $HOME/.scripts/ $GITDIR/.scripts/ --delete
